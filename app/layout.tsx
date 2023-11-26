d2c09cb17b8f8adae756d2fb5c1935ee4fae3021
Ms_Mariga <77338790+Mariga-M@users.noreply.github.com>
2023-11-26 22:00:58 +0300

    app layout and app providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,13 @@ import { SiteBlob } from "@/components/site-blob"
 import { SiteFooter } from "@/components/site-footer"
 import { SiteHeader } from "@/components/site-header"
 
-export const metadata: Metadata = {}
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico"
+  }
+}
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -26,9 +32,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
             fontSans.variable
           )}
         >
+          <Providers>
           <div className="relative flex min-h-screen flex-col">
+            <SiteHeader />
+            <SiteBlob />
             <div className="flex-1">{children}</div>
+            <SiteFooter />
           </div>
+          </Providers>
         </body>
       </html>
     </>
