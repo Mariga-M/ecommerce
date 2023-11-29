3cee2fe0077600aef151d7665b666b076e868e90
Ms_Mariga <77338790+Mariga-M@users.noreply.github.com>
2023-11-29 14:53:39 +0300

    sort, filter and search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,40 @@ import { ProductGrid } from "@/components/product-grid"
 import { ProductSort } from "@/components/product-sort"
 import { seedSanityData } from "@/lib/seed"
 
-interface Props {}
+interface Props {
+  searchParams: {
+    date?: string
+    price?: string
+    color?: string
+    category?: string
+    size?: string
+    search?: string
+  }
+}
 
-export default async function Page() {
+export default async function Page({ searchParams }: Props) {
+
+  const { date = "desc", price, color, category, size, search } = searchParams
+
+  // sort by
+  const priceOrder = price ? `| order(price ${price})` : ""
+  const dateOrder = date ? `| order(_createdAt ${date})` : ""
+  const order = `${priceOrder}${dateOrder}`
+
+  // filter
+  const productFilter = `_type == "product"`
+  const colorFilter = color ? `&& "${color}" in colors` : ""
+  const categoryFilter = category ? `&& "${category}" in categories` : ""
+  const sizeFilter = size ? `&& "${size}" in sizes` : ""
+
+  const searchFilter = search ? `&& name match "${search}"` : ""
+
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
+
+
+
  const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"]{
+    groq`${filter}${order}{
       _id,
       _createdAt,
       name,
