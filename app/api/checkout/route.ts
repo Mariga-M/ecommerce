199b80ac695b8e62c780111fbd0ba98a913d2ad8
Ms_Mariga <77338790+Mariga-M@users.noreply.github.com>
2023-12-01 10:13:18 +0300

    cart summary and stripe integration

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -5,4 +5,25 @@ import { validateCartItems } from "use-shopping-cart/utilities"
 import { inventory } from "@/config/inventory"
 import { stripe } from "@/lib/stripe"
 
-export async function POST() {}
+export async function POST(req:Request) {
+    const cartDetails = await req.json()
+    const lineItems = validateCartItems(inventory, cartDetails)
+
+    const origin = req.headers.get('origin')
+    const session = await stripe.checkout.sessions.create({
+        submit_type:"pay",
+        mode:"payment",
+        payment_method_types:["card"],
+        line_items: lineItems,
+        // shipping_address_collection:{
+        //     allowed_countries: ["US"]
+        // },
+        shipping_options: [{
+            shipping_rate: "shr_1OIPy7HFureDacCUbrOq6ciw"
+        }],
+        // billing_address_collection:"auto",
+        success_url:`${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url:`${origin}/cart`
+    })
+    return NextResponse.json(session)
+}
